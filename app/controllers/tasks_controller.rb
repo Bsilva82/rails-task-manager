99c804c2964fb850e4729940f894671a0f8d1f03
Bruno Silva <info.bsilva@gmail.com>
2021-03-16 16:32:12 +0100

    Rails - Day 2 - Exo 2 - Point 6 - OK

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,17 @@ class TasksController < ApplicationController
   @task.save
   end
 
+  def edit
+  @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # Liste des tasks
-  get "tasks", to: "tasks#index"
+  get "tasks", to: "tasks#index", as: :tasks
   
   #Create a new task
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new_task
   post "tasks", to: "tasks#create"
 
   #Details of a task
   get "tasks/:id", to: "tasks#show", as: :task
-
-
   
+  #Edit task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  
+  #Update the edited task
+  patch "tasks/:id", to: "tasks#update"
 
 end
